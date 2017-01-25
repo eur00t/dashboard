fb597b3c0540653a0af67214c0975e45673de42e
Taras Yevtushenko <taras.evt@gmail.com>
2017-01-25 23:09:40 +0200

    Reconnect immediately only if was connected before

--- client_main.ml
+++ client_main.ml
@@ -2,20 +2,32 @@ let get_websocket url =
     let url_js = Js.string url in
     new%js WebSockets.webSocket url_js;;
 
-let retry_initial_s = 5
+let retry_initial_s = 10
+
+let log str = Firebug.console##log (Js.string str)
 
 let attach_handlers ws retry_s connect =
+    let connected_flag = ref false in
     ws##.onopen := Dom.handler begin
         fun e ->
+            connected_flag := true;
             Dispatcher_client.Client.initial_request Dispatcher_client.client ws;
-            Firebug.console##log (Js.string "Connected");
+            log "Connected";
             Js._true
     end;
 
     ws##.onclose := Dom.handler begin
         fun e ->
-            Firebug.console##log (Js.string ("Disconnected, retrying in " ^ (string_of_int retry_s) ^ "s"));
-            connect retry_s;
+            log "Disconnected";
+            if !connected_flag then begin
+                log "Trying to reconnect immediately";
+                connect retry_s
+            end else begin
+                log ("Trying to reconnect in " ^ (string_of_int retry_s) ^ "s");
+                Js.Unsafe.global##setTimeout (Js.wrap_callback begin
+                    fun () -> connect retry_s
+                end) (Js.number_of_float (float_of_int (retry_s * 1000)));
+            end;
             Js._true
     end;
 
